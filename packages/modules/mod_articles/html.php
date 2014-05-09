5770f23343ec62df49c64f2ddaf83ba05efb3b46
jvdheijden <joep.van.der.heijden@moyoweb.nl>
2014-05-09 15:07:57 +0200

    Assigned params and showtitle

--- packages/modules/mod_articles/html.php
+++ packages/modules/mod_articles/html.php
@@ -7,15 +7,11 @@ class ModArticlesHtml extends ModDefaultHtml
      */
     public function display()
 	{
-//        $taxonomies = $this->getService('com://site/taxonomy.model.taxonomies')->type(array('article', 'event'))->limit($this->module->params->limit)->getList();
-//
-//		$this->assign('taxonomies', $taxonomies);
-//		$this->assign('limit', $this->module->params->limit);
-//        $this->assign('total', $this->getService('com://site/taxonomy.model.taxonomies')->type(array('article', 'event'))->getTotal());
-
 		$article = $this->getService('com://site/articles.model.articles')->id($this->module->params->id)->getItem();
 
 		$this->assign('article', $article);
+		$this->assign('params', $this->module->params);
+		$this->assign('showtitle', $this->module->showtitle);
 
 		return parent::display();
 	}
