5e8fd5ae5d51875d5fe75a37b8829ea87be00c4b
Dave Li <dave.li@imagegroup.nl>
2014-06-23 13:01:17 +0200

    Added configurable database behaviors.

--- packages/component/components/com_articles/databases/tables/articles.php
+++ packages/component/components/com_articles/databases/tables/articles.php
@@ -1,43 +1,6 @@
 <?php
-/**
- * ComArticles
- *
- * @author      Joep van der Heijden <joep.van.der.heijden@moyoweb.nl>
- */
 
-class ComArticlesDatabaseTableArticles extends KDatabaseTableDefault
+class ComArticlesDatabaseTableArticles extends ComMoyoDatabaseTableDefault
 {
-    protected function _initialize(KConfig $config)
-    {
-		$relationable = $this->getBehavior('com://site/taxonomy.database.behavior.relationable',
-			array(
-				'ancestors' => array(
-					'category' => array(
-						'identifier' => 'com://site/makundi.model.categories',
-					),
-					'topic' => array(
-						'identifier' => 'com://site/makundi.model.categories',
-					),
-					'tags' => array(
-						'identifier' => 'com://admin/terms.model.tags',
-					),
-				),
-				'descendants' => array(
-					'articles' => array(
-						'identifier' => 'com://site/articles.model.articles',
-					)
-				)
-			)
-		);
 
-        $config->append(array(
-            'behaviors' => array(
-                $relationable,
-				'com://admin/cck.database.behavior.elementable',
-                'com://admin/translations.database.behavior.translatable',
-            )
-        ));
-
-        parent::_initialize($config);
-    }
 }
