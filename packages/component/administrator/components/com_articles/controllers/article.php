a829b5c0ab033c74aa1f5ee424a72f073c730f76
Jasper van Rijbroek <jasper@jvar.nl>
2014-12-29 10:10:53 +0100

    Added the autosaveable behavior to the controller of articles.

--- packages/component/administrator/components/com_articles/controllers/article.php
+++ packages/component/administrator/components/com_articles/controllers/article.php
@@ -6,6 +6,12 @@ class ComArticlesControllerArticle extends ComDefaultControllerDefault
     {
         $this->mixin($this->getService('com://admin/kutafuta.controller.behavior.indexable'));
         $this->mixin($this->getService('com://admin/moyo.controller.behavior.copyable'));
+        
+        $config->append(array(
+            'behaviors' => array(
+                'com://admin/cck.controller.behavior.autosavable'
+            )
+        ));
 
         parent::_initialize($config);
 	}
